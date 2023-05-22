ba7b8789d0dd6bffcba9b3ff56b93e1a7c5c2866
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2023-05-22 12:07:40 -0300

    Begin proof of concept

---
+++ ProofOfConcept/Program.cs
@@ -0,0 +1,32 @@
+﻿// This program will be run from cmd as a proof of concept
+// The first argument is the data.win location
+// The second argument is the mod folder location
+
+using Microsoft.Extensions.Configuration;
+using UndertaleModLib;
+using UndertaleModLib.Decompiler;
+
+string gameDataPath = args[0];
+string modFolder = args[1];
+string modsConfig = $"{modFolder}/mods.ini";
+
+UndertaleData gameData;
+
+try
+{
+    using (var stream = new FileStream(gameDataPath, FileMode.Open, FileAccess.ReadWrite))
+        gameData = UndertaleIO.Read(stream);
+} catch 
+{
+    Console.WriteLine("Error reading game data");
+    return;
+}
+
+foreach(string folder in Directory.GetDirectories(modFolder))
+{
+    var modConfig = new ConfigurationBuilder().AddIniFile($"{folder}/mod.ini").Build().GetSection("Loader");
+    if (modConfig["enabled"] == "false")
+        continue;
+
+    Console.WriteLine($"Loading mod {modConfig["name"]}");    
+}
